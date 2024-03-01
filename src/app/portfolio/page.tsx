88e5841845f31ae0c9fa152a992b16a926108b3a
pandeeswari1926 <apandeeswari1926@gmail.com>
2024-03-01 12:19:28 +0530

    sada

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image';
 import Web from './web';
 import WebDesign from './webDesign';
+import Iot from './iot';
 import Appdevelopment from './Appdevelopment'
 import portfolio1 from '../../../public/wgs gifs (12).gif';
 import portfolio2 from '../../../public/wgs gifs (13).gif';
@@ -13,14 +14,13 @@ import message1 from '../../../public/message2.png';
 import message2 from '../../../public/Rectangle 129.png';
 import message3 from '../../../public/message5.png'
 import message4 from '../../../public/message4.png';
-import Iot from './iot';
 
 
 const Page = () => {
     return (
         <>
             {/* first section */}
-            <div className='bg-portfolio bg-cover  overflow-hidden'>
+            <div className='bg-portfolio bg-center overflow-hidden'>
                 <div className='pt-28 px-10'>
                     <div className='text-center '>
                         <h1 className='text-white md:text-8xl text-xl font-semibold leading-snug'>TAKE YOUR IDEAS TO THE <br className='md:block hidden' />NEXT LEVEL</h1><br />
@@ -29,26 +29,26 @@ const Page = () => {
                     </div>
                     <div className=''>
                         <div>
-                            <div className='flex md:flex-row flex-col justify-center '>
-                                <div className='md:w-96 w-full relative top-20 md:-left-20 md:mx-0 mx-20'>
+                            <div className='flex md:flex-row flex-col justify-around  md:gap-0 gap-10'>
+                                <div className='md:w-80 w-full relative md:top-20 top-20 md:ml-10 ml-20'>
                                     <Image src={portfolio5} alt='' className='w-[55%] rounded-full border-8 border-gray-300 drop-shadow-2xl border-opacity-25' />
                                     <Image src={message2} alt='' className='absolute w-[50%] -mt-10 -ml-28' />
                                 </div>
-                                <div className='md:w-96 w-full relative md:left-64 md:top-20 top-28 md:mx-0 mx-10'>
+                                <div className='md:w-80 w-full relative md:left-24 md:top-20 top-28 md:mx-0 mx-6'>
                                     <Image src={portfolio4} alt='' className='w-[55%] rounded-full border-8 border-gray-300 drop-shadow-2xl border-opacity-25' />
                                     <Image src={message1} alt='' className='absolute w-[50%] -mt-10 ml-36' />
                                 </div>
                             </div>
-                            <div className='flex md:flex-row flex-col justify-evenly items-center pb-20 '>
-                                <div className='md:w-96 w-full relative md:top-32 top-36 md:ml-0 ml-40'>
+                            <div className='flex md:flex-row flex-col justify-evenly items-center pb-14 md:gap-0 gap-10'>
+                                <div className='md:w-80 w-full relative md:top-20 top-36 md:ml-10 ml-40'>
                                     <Image src={portfolio2} alt='' className='w-[55%] rounded-full border-8 border-gray-300 drop-shadow-2xl border-opacity-25' />
                                     <Image src={message} alt='' className='absolute w-[50%] -mt-10 -ml-28' />
                                 </div>
-                                <div className='md:w-[500px] w-full relative md:mt-24 mt-44  ml-20'>
+                                <div className='md:w-[400px] w-full relative md:-mt-28 mt-40'>
                                     <Image src={portfolio3} alt='' className='md:w-full w-[55%] rounded-full border-[20px] border-gray-300 drop-shadow-2xl border-opacity-25' />
-                                    <Image src={message4} alt='' className='absolute w-[50%] -mt-28 md:left-72 ml-32' />
+                                    <Image src={message4} alt='' className='absolute w-[50%] -mt-20 md:left-72 md:ml-0 ml-32' />
                                 </div>
-                                <div className='md:w-96 w-full relative md:left-36 md:top-32 top-14 md:ml-0 ml-20'>
+                                <div className='md:w-80 w-full relative md:left-36 md:top-16  md:ml-0 ml-20'>
                                     <Image src={portfolio1} alt='' className='w-[55%] rounded-full border-8 border-gray-300 drop-shadow-2xl border-opacity-25' />
                                     <Image src={message3} alt='' className='absolute w-[50%] -mt-10 ml-32' />
                                 </div>
@@ -72,9 +72,9 @@ const Page = () => {
                 <WebDesign />
             </div>
 
-
             {/* sixth section */}
-            <div className='pt-10'>
+
+            <div>
                 <Iot />
             </div>
         </>
