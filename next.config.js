f3ad42e7ed5b5acdeaa1da10fdfc1915a3d90bfc
pandeeswari1926 <apandeeswari1926@gmail.com>
2024-03-13 09:59:45 +0530

    correction

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    //output: "export",
+    // env: {
+    //     API_URL: "http://localhost:3010/api",
+    //     // API_URL: "https://antiapi.w8loss.uk/api",
+    // },
+};
+
+module.exports = nextConfig;
