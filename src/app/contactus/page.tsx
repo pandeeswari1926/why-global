36a370043611140d0e6d647bbfc5cf31084ec2c6
tajudeen63 <mohdtaj6384@gmail.com>
2024-04-25 16:12:55 +0530

    contact page

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import Image from 'next/image'
 
 const page = () => {
   return (
-    <div className='bg-getin h-96 w-96'>
-       <div className=''> </div>
-    </div>
+    
   )
 }
 
