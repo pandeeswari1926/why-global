4162350da76b4da0941006b2026a6a2584533bda
Wasimwhy <wasim@whyglobalservices.com>
2024-04-29 14:56:44 +0530

    portfolio

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -138,19 +138,6 @@ const Page = () => {
       {/* sixth section */}
       <div className="overflow-hidden"><Iot /></div>
 
-      <div className="flex flex-col justify-center items-center overflow-hidden py-5">
-        <div className="relative w-full lg:h-[236px] h-[250px]  px-10">
-          <Image src={bgorange1} alt="" className="w-full h-full object-cover" />
-          <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2  text-center space-y-10">
-            <h2 className="font-semibold sm:text-5xl text-3xl text-white">
-              Connect with Us!
-            </h2>
-            <button className="bg-white drop-shadow-xl text-[#FF8900] rounded-full sm:px-32 px-20 py-4 font-semibold sm:text-xl text-lg">
-              Get Started
-            </button>
-          </div>
-        </div>
-      </div>
     </>
   );
 };
