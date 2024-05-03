5cc2fdb73d5849fa433932c1a3ac826772951701
bobert13gomez <bobertgomez13@gmail.com>
2024-05-03 15:32:27 +0530

    bob

---
+++ src/app/news/[Individual]/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+import React from "react";
+import { FaCalendar } from "react-icons/fa";
+
+function Page({paramsl}:any){
+    console.log(paramsl)
+
+    return(
+        <div className="h-screen w-full">
+         
+            <div className="w-full h-full flex lg:flex-row flex-col relative p-5">
+                <div className="lg:w-[70%] w-full">
+                    <div className="w-full">
+                        <p className="md:text-3xl text-xl font-semibold">Heading</p>
+                        <div className="lg:flex-row flex flex-col w-full justify-between items-center">
+                            <p className="text-lg">sunil Kumar</p>
+                            <p className="text-sm date flex gap-2  items-center"><span>12/12/12 Sat</span> <FaCalendar className="size-4" /></p>
+                        </div>
+                        <img src="./profile.png"></img>
+                        <div className="flex flex-col gap-5">
+                            <p>Content</p>
+                            <p>Content</p>
+                            <p>Content</p>
+                        </div>
+
+
+                    </div>
+                </div>
+                <div className="lg:w-[30%] w-full sticky">
+
+                </div>
+              
+            </div>
+            
+        </div>
+    )
+}
+export default Page;
