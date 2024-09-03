1a3c2efc0eb342a0ec0c7cb9b3b360cef365a15e
LincyJo <lincy@whyglobalservices.com>
2024-09-03 18:11:52 +0530

    why choose

--- src/app/Homepage/WhyChoose.jsx
+++ src/app/Homepage/WhyChoose.jsx
@@ -4,109 +4,109 @@ import { FaQuestion } from "react-icons/fa";
 function WhyChooseUs() {
   return (
     <div className='w-full relative overflow-hidden py-5 bg-gray-00'>
-      <img src='/ply.png' className='lg:w-44 w-36 -bottom-5 -left-20 absolute' alt="Decoration" />
-      <img src='/poly.png' className='lg:w-52 w-32 -top-20 -right-5 absolute' alt="Decoration" />
+      <img src='/ply.png' className='lg:w-44 w-36 -bottom-5 -left-20 absolute md:block hidden' alt="Decoration" />
+      <img src='/poly.png' className='lg:w-52 w-32 -top-20 -right-5 absolute ' alt="Decoration" />
 
       <div className='md:py-5 md:pl-10  text-center'>
-        <h2 className='text-3xl md:text-5xl font-bold text-primary flex items-center justify-center'>
+        <h2 className='text-3xl md:text-5xl font-bold text-primary flex items-center justify-center mt-6 md:mt-0'>
           Why Choose Us <FaQuestion className='ml-2 md:text-3xl text-xl' />
         </h2>
       </div>
 
       <div className='flex flex-col lg:flex-row md:px-10 md:gap-[2%] justify-center items-center '>
-        <div className='flex flex-col items-center -mt-10 text-center'>
-        <div className='bg-white shadow-orange-200 rounded-full p-8 shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500'>
+        <div className='flex flex-col items-center md:md:-mt-10 text-center'>
+        <div className='bg-white md:shadow-orange-200 rounded-full p-8 md:shadow-2xl shadow-lg   shadow-orange-200 transition-transform transform hover:scale-110 hover:bg-orange-500'>
         <img src='/Whychooseusimg1.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert' alt="Qualified Professionals" />
           </div>
-          <p className='text-base font-semibold mt-9 text-gray-800'>13+ Years of Expertise</p>
+          <p className='text-base font-semibold md:mt-9 mt-4  text-gray-800'>13+ Years of Expertise</p>
         </div>
 
         <div className='flex flex-col items-center text-center md:mt-[80px]'>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500 '>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg   transition-transform transform hover:scale-110 hover:bg-orange-500 '>
             <img src='/Whychooseusimg2.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert' alt="Proven Track Record" />
           </div>
-          <p className='text-base font-semibold mt-9 text-gray-800'>Proven Track Record</p>
+          <p className='text-base font-semibold md:mt-9 mt-4 text-gray-800'>Proven Track Record</p>
         </div>
 
-        <div className='flex flex-col items-center -mt-10 text-center'>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500'>
+        <div className='flex flex-col items-center md:md:-mt-10 text-center'>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg  transition-transform transform hover:scale-110 hover:bg-orange-500'>
             <img src='/Whychooseusimg3.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert' alt="Qualified Professionals" />
           </div>
-          <p className='text-base font-semibold mt-9 text-gray-800'>Qualified Professionals</p>
+          <p className='text-base font-semibold md:mt-9 mt-4 text-gray-800'>Qualified Professionals</p>
         </div>
 
         <div className='flex flex-col items-center text-center md:mt-[80px]'>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500 '>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg  transition-transform transform hover:scale-110 hover:bg-orange-500 '>
             <img src='/Whychooseusimg4.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert' alt="Cutting-edge Technologies" />
           </div>
-          <p className='text-base font-semibold mt-9 text-gray-800'>Cutting-edge Technologies</p>
+          <p className='text-base font-semibold md:mt-9 mt-4 text-gray-800'>Cutting-edge Technologies</p>
         </div>
 
-        <div className='flex flex-col items-center -mt-10 text-center'>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500'>
+        <div className='flex flex-col items-center md:-mt-10 text-center'>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg  transition-transform transform hover:scale-110 hover:bg-orange-500'>
             <img src='/Whychooseusimg5.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert' alt="Client-Centric Approach" />
           </div>
-          <p className='text-base font-semibold mt-9 text-gray-800'>Client-Centric Approach</p>
+          <p className='text-base font-semibold md:mt-9 mt-4 text-gray-800'>Client-Centric Approach</p>
         </div>
         <div className='flex flex-col items-center text-center md:mt-[80px]'>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500 '>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg  transition-transform transform hover:scale-110 hover:bg-orange-500 '>
             <img src='/Whychooseusimg6.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert' alt="On-time Project Delivery" />
           </div>
           <p className='text-base font-semibold mt-9 text-gray-800'>On-time Project Delivery</p>
         </div>
-        <div className='flex flex-col items-center -mt-10 text-center'>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500 '>
+        <div className='flex flex-col items-center md:-mt-10 text-center'>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg  transition-transform transform hover:scale-110 hover:bg-orange-500 '>
             <img src='/Whychooseusimg7.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert'  alt="Tailor-made Products"  />
           </div>
           <p className='text-base font-semibold mt-9 text-gray-800'>Tailor-made Products</p>
         </div>
         </div>
-        <div className='flex flex-col md:mt-12 lg:flex-row md:px-10 md:gap-[2%] justify-center items-center'>
+        <div className='flex flex-col md:mt-12 lg:flex-row md:px- md:gap-[4%] justify-center items-center'>
         <div className='flex flex-col items-center text-center md:-mt-12'>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500'>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg  transition-transform transform hover:scale-110 hover:bg-orange-500'>
             <img src='/Whychooseusimg8.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert' alt="24/7 Help Desk" />
           </div>
           <p className='text-base font-semibold mt-9 text-gray-800'>24/7 Help Desk</p>
         </div>
 
         <div className='flex flex-col items-center  text-center md:mt-7'>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500'>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg  transition-transform transform hover:scale-110 hover:bg-orange-500'>
             <img src='/Whychooseusimg9.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert' alt="Software Updates" />
           </div>
-          <p className='text-base font-semibold md:mt-9 text-gray-800'>Software Updates</p>
+          <p className='text-base font-semibold md:mt-9 mt-4 text-gray-800'>Software Updates</p>
         </div>
 
         <div className='flex flex-col items-center text-center md:-mt-12'>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500'>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg  transition-transform transform hover:scale-110 hover:bg-orange-500'>
             <img src='/Whychooseusimg10.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert' alt="Annual Maintenance" />
           </div>
-          <p className='text-base font-semibold mt-9 text-gray-800'>Annual Maintenance</p>
+          <p className='text-base font-semibold md:mt-9 mt-4 text-gray-800'>Annual Maintenance</p>
         </div>
 
         <div className='flex flex-col items-center text-center '>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500 '>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg  transition-transform transform hover:scale-110 hover:bg-orange-500 '>
             <img src='/Whychooseusimg4.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert' alt="Training,Knowledge Transfer" />
           </div>
-          <p className='text-base font-semibold mt-9 text-gray-800'>Training,Knowledge Transfer</p>
+          <p className='text-base font-semibold md:mt-9 mt-4 text-gray-800'>Training,<br/>Knowledge Transfer</p>
         </div>
 
         <div className='flex flex-col items-center text-center md:-mt-12'>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500 '>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg  transition-transform transform hover:scale-110 hover:bg-orange-500 '>
             <img src='/Whychooseusimg5.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert' alt="Security Audit" />
           </div>
-          <p className='text-base font-semibold mt-9 text-gray-800'>Security Audit</p>
+          <p className='text-base font-semibold md:mt-9 mt-4 text-gray-800'>Security Audit</p>
         </div>
         <div className='flex flex-col items-center text-center '>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500'>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg  transition-transform transform hover:scale-110 hover:bg-orange-500'>
             <img src='/Whychooseusimg6.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert' alt="Remote Support" />
           </div>
-          <p className='text-base font-semibold mt-9 text-gray-800'>Remote Support</p>
+          <p className='text-base font-semibold md:mt-9 mt-4 text-gray-800'>Remote Support</p>
         </div>
         <div className='flex flex-col items-center text-center md:-mt-12'>
-          <div className='bg-white shadow-orange-200 rounded-full p-8  shadow-2xl transition-transform transform hover:scale-110 hover:bg-orange-500 '>
+          <div className='bg-white shadow-orange-200 rounded-full p-8  md:shadow-2xl shadow-lg  transition-transform transform hover:scale-110 hover:bg-orange-500 '>
             <img src='/Whychooseusimg7.png' className='w-12 h-12 transition-colors duration-300 hover:filter hover:brightness-0 hover:invert'  alt="Dedication Client Relationship Manager"  />
           </div>
-          <p className='text-base font-semibold mt-9 text-gray-800'>Dedication Client Relationship Manager</p>
+          <p className='text-base font-semibold md:mt-9 mt-4 text-gray-800'>Dedication Client <br/> Relationship Manager</p>
         </div>
         </div>
 
