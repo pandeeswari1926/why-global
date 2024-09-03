890c1f2d3288ea36eb396ca2d457bf454738f5df
Hussainkani <mohammed@whyglobalservices.com>
2024-09-03 19:37:17 +0530

    Ui changes in Our portfolio section

--- src/app/Homepage/NewServices.tsx
+++ src/app/Homepage/NewServices.tsx
@@ -1,21 +1,97 @@
 import React from "react";
 import { PiMedalFill } from "react-icons/pi";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
 
 const TimelineCard = () => {
-    return (
-        <>
-            <div className="bg-gray-50 py-5 overflow-hidden">
-                <div className='md:py-8 md:pl-10 p-5'>
-                    <p className='sixHundred:text-5xl xs:text-4xl text-2xl lg:justify-start justify-center flex items-center font-bold'>Our Services</p>
-                </div>
-                <div className="w-full h-full grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 lg:justify-around justify-center items-center lg:items-start py-5 px-5">
+  const services = [
+    {
+      title: "Web Designing",
+      description:
+        "Crafting visually compelling and responsive websites that enhance user experience and reflect your brand’s identity.",
+      iconSrc: "/newService1.png",
+      bgImage: "url('/wgs gifs (10) 1.png')",
+      details: [
+        "Responsive Website Design",
+        "UI/UX Design",
+        "Website Maintenance and Support",
+      ],
+      bg_color: "			#801818",
+    },
+    {
+      title: "Web Development",
+      description:
+        "Building robust, scalable websites with cutting-edge technologies, ensuring functionality and seamless performance.",
+      iconSrc: "/newService4.png",
+      bgImage: "url('/wgs gifs (10) 1.png')",
+      details: [
+        "Custom Web App Development",
+        "E-commerce Website Development",
+        "CMS Development",
+      ],
+      bg_color: "	#CC8899",
+    },
+    {
+      title: "Mobile App Development",
+      description:
+        "Developing intuitive and user-friendly mobile apps tailored to meet your business needs across all platforms.",
+      iconSrc: "/newService2.png",
+      bgImage: "url('/wgs gifs (10) 1.png')",
+      details: [
+        "iOS App Development",
+        "Android App Development",
+        "Cross-Platform App Development",
+      ],
+      bg_color: "		#AD4D8C",
+    },
+    {
+      title: "Digital Marketing",
+      description:
+        "Boosting your online presence with data-driven strategies, SEO, and social media marketing to drive traffic and conversions.",
+      iconSrc: "/newService5.png",
+      bgImage: "url('/wgs gifs (10) 1.png')",
+      details: [
+        "Search Engine Optimization",
+        "Social Media Marketing",
+        "Search Engine Marketing",
+        "Content Marketing",
+        "Affiliate Marketing",
+      ],
+      bg_color: "	#42B3AE",
+    },
+    {
+      title: "Internet of Things (IoT)",
+      description:
+        "Boosting your online presence with data-driven strategies, SEO, and social media marketing to drive traffic and conversions.",
+      iconSrc: "/newService3.png",
+      bgImage: "url('/wgs gifs (10) 1.png')",
+      details: [
+        "IoT Consultation and Strategy",
+        "IoT Device Integration",
+        "IoT Data Analytics",
+      ],
+      bg_color: "	#E52B50",
+    },
+  ];
 
-                   
+  return (
+    <>
+      <div
+        className="bg-gray-50 py-5  w-full h-full bg-cover object-fill bg-no-repeat overflow-hidden"
+        style={{ backgroundImage: "url('/bg_services.png')" }}
+      >
+        <div className="md:py-8 md:pl-10 p-5">
+          <p className="sixHundred:text-5xl xs:text-4xl text-2xl lg:justify-start justify-center flex items-center font-bold">
+            Our Services
+          </p>
+        </div>
+        {/* <div className="w-full h-full flex lg:flex-row flex-col lg:justify-around justify-center items-center lg:items-start py-5 px-5">
+
+                    <div>
                         <div>
                             <div className='flex justify-center sm:justify-start items-center'>
                                 <div className='bg-slate-50 z-10 drop-shadow-xl rounded-full w-16 h-16 flex justify-center items-center'>
                                     <div className='bg-orange-500  flex justify-center items-center rounded-full drop-shadow-lg w-11 h-11 p-2'>
-                                        {/* <PiMedalFill className='p-1 size-12 text-white' /> */}
+                                      
                                         <img src="/newService1.png"/>
                                     </div>
 
@@ -57,7 +133,7 @@ const TimelineCard = () => {
                             <div className='flex justify-center sm:justify-start items-center'>
                                 <div className='bg-slate-50 z-10 drop-shadow-xl rounded-full w-16 h-16 flex justify-center items-center'>
                                     <div className='bg-orange-500 flex justify-center items-center rounded-full drop-shadow-lg w-11 h-11 p-3'>
-                                        {/* <PiMedalFill className='p-1 size-12 text-white' /> */}
+                                      
                                         <img src="/newService2.png"/>
                                     </div>
 
@@ -99,7 +175,7 @@ const TimelineCard = () => {
                             <div className='flex justify-center sm:justify-start items-center'>
                                 <div className='bg-slate-50 z-10 drop-shadow-xl rounded-full w-16 h-16 flex justify-center items-center'>
                                     <div className='bg-orange-500 flex justify-center items-center rounded-full drop-shadow-lg w-11 h-11 p-2'>
-                                        {/* <PiMedalFill className='p-1 size-12 text-white' /> */}
+                                      
                                         <img src="/newService3.png"/>
                                     </div>
 
@@ -136,14 +212,14 @@ const TimelineCard = () => {
                                 </div>
                             </div>
                         </div>
-                   
+                    </div>
 
-                    
+                    <div>
                         <div>
                             <div className='flex justify-center sm:justify-start items-center'>
                                 <div className='bg-slate-50 z-10 drop-shadow-xl rounded-full w-16 h-16 flex justify-center items-center'>
                                     <div className='bg-orange-500 flex justify-center items-center rounded-full drop-shadow-lg w-11 h-11 p-2'>
-                                        {/* <PiMedalFill className='p-1 size-12 text-white' /> */}
+                                     
                                         <img src="/newService4.png"/>
                                     </div>
 
@@ -186,7 +262,6 @@ const TimelineCard = () => {
                             <div className='flex justify-center sm:justify-start items-center'>
                                 <div className='bg-slate-50 z-10 drop-shadow-xl rounded-full w-16 h-16 flex justify-center items-center'>
                                     <div className='bg-orange-500 flex justify-center items-center rounded-full drop-shadow-lg w-11 h-11 p-2'>
-                                        {/* <PiMedalFill className='p-1 size-12 text-white' /> */}
                                         <img src="/newService5.png"/>
                                     </div>
 
@@ -238,11 +313,68 @@ const TimelineCard = () => {
                                 </div>
                             </div>
                         </div>
-                   
+                    </div>
+                </div> */}
+        <div className="w-full mt-4 grid grid-cols-1 gap-10 px-4 sm:px-10 md:px-16 lg:px-20 lg:grid-cols-3 md:grid-cols-2">
+          {services.map((service, index) => (
+            <div
+              key={index}
+              className="relative flex h-72 bg-white w-full flex-col items-center justify-center shadow-lg rounded-lg group"
+            >
+              <div className="flex flex-col items-center justify-center">
+                <div className="mt-2">
+                  <div className="bg-slate-50 z-10 drop-shadow-xl rounded-full flex justify-center items-center">
+                    <div
+                      className="flex justify-center items-center rounded-full drop-shadow-lg w-14 h-14 p-2"
+                      style={{ backgroundColor: service.bg_color }}
+                    >
+                      <img
+                        src={service.iconSrc}
+                        className="w-10 p-1 h-10"
+                        alt={`${service.title} Icon`}
+                      />
+                    </div>
+                  </div>
                 </div>
+                <div className="flex flex-col items-center justify-center px-4 sm:px-5 py-4">
+                  <h2 className="font-semibold text-lg md:text-xl text-center">
+                    {service.title}
+                  </h2>
+                  <p className="text-sm md:text-base mt-2 text-center">
+                    {service.description}
+                  </p>
+                </div>
+              </div>
+              {/* Hover Effect Overlay */}
+              <div className="absolute inset-0 overflow-hidden rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+                <div
+                  className="w-full h-full bg-center bg-no-repeat"
+                  style={{ backgroundImage: service.bgImage }}
+                >
+                  <div
+                    className="absolute inset-0 opacity-90"
+                    style={{ backgroundColor: service.bg_color }}
+                  ></div>
+                  <div className="relative flex flex-col gap-2 px-4 items-start bg-transparent justify-center h-full">
+                    {service.details.map((detail, i) => (
+                      <div key={i} className="flex gap-2 items-center">
+                        <div className="bg-white text-gray-600 rounded-full">
+                          <BsFillArrowRightCircleFill className="size-6 rounded-full" />
+                        </div>
+                        <p className="cursor-pointer text-sm md:text-lg font-semibold text-white transition-transform duration-300 ease-out transform hover:scale-95">
+                          {detail}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
             </div>
-        </>
-    );
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TimelineCard;
--- src/app/Homepage/OurPortfolio.tsx
+++ src/app/Homepage/OurPortfolio.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { FaArrowRight } from 'react-icons/fa';
-import { FaArrowLeft } from 'react-icons/fa6';
+import React, { useEffect, useState } from "react";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
 const OurPortfolio = () => {
   const ourPortfolio = [
-    { images: 'OurPortfolioimg1.png' },
-    { images: 'OurPortfolioimg2.png' },
-    { images: 'OurPortfolioimg3.png' },
-    { images: 'OurPortfolioimg4.png' },
-    { images: 'OurPortfolioimg5.png' },
+    { images: "OurPortfolioimg1.png", title : "Project One" },
+    { images: "OurPortfolioimg2.png", title : "Project two" },
+    { images: "OurPortfolioimg3.png", title : "Project Three" },
+    { images: "OurPortfolioimg4.png" , title : "Project Four"},
+    { images: "OurPortfolioimg5.png", title : "Project Five" },
   ];
 
   const [activeIndex, setActiveIndex] = useState(2); // Start with the third image as the center focus
@@ -16,89 +15,107 @@ const OurPortfolio = () => {
   const handleNext = () => {
     setActiveIndex((prevIndex) => (prevIndex + 1) % ourPortfolio.length);
   };
-  const[hover,sethover]=useState(true)
-useEffect(()=>{
 
-  if(hover){
-    let interval=setInterval(()=>{
-      setActiveIndex((prevIndex) => (prevIndex + 1) % ourPortfolio.length);
-    },3000)
-    
-    return()=>clearInterval(interval)
-  }
+  const [hover, setHover] = useState(true);
 
-  
-},[hover])
+  useEffect(() => {
+    if (hover) {
+      let interval = setInterval(() => {
+        setActiveIndex((prevIndex) => (prevIndex + 1) % ourPortfolio.length);
+      }, 3000);
+
+      return () => clearInterval(interval);
+    }
+  }, [hover]);
+
   const handlePrev = () => {
     setActiveIndex(
-      (prevIndex) =>
-        (prevIndex - 1 + ourPortfolio.length) % ourPortfolio.length
+      (prevIndex) => (prevIndex - 1 + ourPortfolio.length) % ourPortfolio.length
     );
   };
 
   return (
-    <article className='relative overflow-hidden w-full h-full py-5'>
-      <div className='md:py-8 md:pl-10 p-5 bg-gray-100'>
-            <p className=' sixHundred:text-5xl xs:text-4xl text-2xl lg:justify-start justify-center flex items-center font-bold'>Our Portfolio</p>
-        </div>
-        <img src="/width_200.png" alt="" className="w-44 absolute right-0 top-0 -mt-16 -mr-20 sm:block hidden"/>
-    <section className="pb-5 md:pb-10 bg-gray-100 relative   min-h-[90vh] sixHundred:min-h-[100vh] flex items-center justify-center">
-       
-      <div className="w-[90%] mx-auto flex items-center  justify-center h-full relative">
-        {/* Previous Button */}
-        <button
-          onClick={handlePrev}
-          className="absolute left-0 bg-gray-800 text-white border-2 hover:border-primarycolor duration-200 transition-all border-transparent p-5 hover:bg-transparent hover:text-primarycolor rounded-full z-10"
-        >
-          <FaArrowLeft />
-        </button>
-
-        <div className="relative w-full flex items-center  justify-center h-full">
-          {ourPortfolio.map((item, index) => {
-
-            let position = index - activeIndex;
-
-            if (position < -2) {
-              position += ourPortfolio.length;
-            } else if (position > 2) {
-              position -= ourPortfolio.length;
-            }
-
-            let scale = 1 - Math.abs(position) * 0.2;
-            let zIndex = 5 - Math.abs(position); 
-
-            return (
-              <div
-                key={index}
-                onMouseEnter={()=>sethover(false)}
-                onMouseLeave={()=>sethover(true)}
-                className={`absolute transition-all    sm:min-h-[100vh] h-[85vh] duration-500`}
-                style={{
-                  transform: `translateX(${position * 150}px) scale(${scale})`,
-                  zIndex,
-                  opacity: position === -3 || position === 3 ? 0 : 1,
-                }}
-              >
-                <img
-                  className={`rounded-lg overflow-hidden relative ${index==activeIndex?'bg-red-500':''}  shadow-2xl w-full h-full`} // Adjust height for images
-                  src={item.images}
-                  alt={`Portfolio ${index + 1}`}
-                  style={{ width: '320px' }} // Adjust width as needed
-                />
-              </div>
-            );
-          })}
-        </div>
-
-        {/* Next Button */}
-        <button
-          onClick={handleNext}
-          className="absolute right-0 bg-gray-800 text-white border-2 hover:border-primarycolor duration-200 transition-all border-transparent p-5 hover:bg-transparent hover:text-primarycolor rounded-full z-10"
-        >
-          <FaArrowRight />
-        </button>
+    <article
+      className="relative overflow-hidden bg-cover bg-no-repeat w-full h-screen py-5"
+      style={{ backgroundImage: "url('/bg_profilo.png')" }}
+    >
+      <div className="md:py-8 md:pl-10 p-5">
+        <p className="sixHundred:text-5xl xs:text-4xl text-2xl lg:justify-start justify-center flex items-center font-bold">
+          Our Portfolio
+        </p>
       </div>
-    </section>
+      <img
+        src="/width_200.png"
+        alt=""
+        className="w-44 absolute right-0 top-0 -mt-16 -mr-20 sm:block hidden"
+      />
+      <section className="pb-5 md:pb-1 relative top-[-5rem] min-h-[90vh] sixHundred:min-h-[100vh] flex items-center justify-center">
+        <div className="w-[90%] mx-auto flex items-center justify-center h-full relative">
+          {/* Previous Button */}
+          <button
+            onClick={handlePrev}
+            className="absolute left-0 bg-gray-800 text-white border-2 hover:border-primarycolor duration-200 transition-all border-transparent p-5 hover:bg-transparent hover:text-primarycolor rounded-full z-10"
+          >
+            <FaArrowLeft />
+          </button>
+
+          <div className="relative w-full flex items-center justify-center h-full">
+            {ourPortfolio.map((item, index) => {
+              let position = index - activeIndex;
+
+              if (position < -2) {
+                position += ourPortfolio.length;
+              } else if (position > 2) {
+                position -= ourPortfolio.length;
+              }
+
+              let scale = 1 - Math.abs(position) * 0.2;
+              let zIndex = 5 - Math.abs(position);
+
+              return (
+                <div
+                  key={index}
+                  onMouseEnter={() => setHover(false)}
+                  onMouseLeave={() => setHover(true)}
+                  className={`absolute transition-transform duration-500 ease-in-out`}
+                  style={{
+                    transform: `translateX(${
+                      position * 150
+                    }px) scale(${scale})`,
+                    zIndex,
+                    opacity: position === -3 || position === 3 ? 0 : 1,
+                    filter: index === activeIndex ? "none" : "blur(4px)",
+                    margin: "0 20px", // Gap between images
+                  }}
+                >
+                  <img
+                    className="rounded-t-lg shadow-2xl w-full h-full object-cover"
+                    src={item.images}
+                    alt={`Portfolio ${index + 1}`}
+                    // style={{ width: "320px", height: "400px" }}
+                    style={{
+                      width: '370px',
+                      height: '500px',
+                      objectPosition: 'top', // Aligns the image to the top of its container
+                    }} // Adjust width and height as needed
+                  />
+                  <div className="p-2 text-center bg-primarycolor text-white">
+                    <h1>{item.title}</h1>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          {/* Next Button */}
+          <button
+            onClick={handleNext}
+            className="absolute right-0 bg-gray-800 text-white border-2 hover:border-primarycolor duration-200 transition-all border-transparent p-5 hover:bg-transparent hover:text-primarycolor rounded-full z-10"
+          >
+            <FaArrowRight />
+          </button>
+        </div>
+      </section>
     </article>
   );
 };
